8190e873df4da24b34d289171b182ef2e929243d
a <a@hoge.com>
2025-01-17 22:40:12 +0900

    ライブラリ的なフォルダを括りだした。 Elin_Libsにライブラリを移動し、config.batでシンボリックリンクを貼って参照するような形に変更した。 テンプレート含め、現状の全プロジェクトを同様の処理変更。（AutoExplorerは自分のじゃないので置いとく）

--- Elin_ModTemplate/config.bat
+++ Elin_ModTemplate/config.bat
@@ -3,13 +3,22 @@
 ::---------------.
 :: Elinのインストールフォルダ.
 set ELIN_PATH="C:\Program Files (x86)\Steam\steamapps\common\Elin"
-:: 自身のMod名.
-set MY_MOD_NAME=Elin_Template
+:: Lib元フォルダ.
+set LIB_SRC_PATH="%~dp0/../Elin_Libs"
+:: Libコピー先フォルダ.
+set LIB_DEST_PATH="%~dp0/src/Lib"
 
 ::---------------.
 :: DLL参照用にシンボリックリンクを貼る.
 ::---------------.
 if not exist %~dp0elin_link\ (
     mklink /D %~dp0elin_link\ %ELIN_PATH% 
+)
+
+::---------------.
+:: Lib参照用にシンボリックリンクを貼る.
+::---------------.
+if not exist %LIB_DEST_PATH% (
+    mklink /D %LIB_DEST_PATH% %LIB_SRC_PATH% 
 )
 
